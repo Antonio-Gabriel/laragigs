8e8ae96f452c94597e7e9dab5afa076c6fd1c047
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-08-21 15:57:41 +0100

    refactor: add image lambda

--- resources/views/components/listing-card.blade.php
+++ resources/views/components/listing-card.blade.php
@@ -2,13 +2,15 @@
 
 <x-card>
     <div class="flex">
-        <img class="hidden w-48 mr-6 md:block" src="{{ asset('images/no-image.png') }}" alt="" />
+        <img class="hidden w-48 mr-6 md:block"
+            src="{{ $listing->logo ? asset('storage/' . $listing->logo) : asset('images/no-image.png') }}"
+            alt="" />
         <div>
             <h3 class="text-2xl">
                 <a href="/listings/{{ $listing->id }}">{{ $listing->title }}</a>
             </h3>
             <div class="text-xl font-bold mb-4">{{ $listing->company }}</div>
-            
+
             <x-listing-tags :tagsCsv="$listing->tags" />
 
             <div class="text-lg mt-4">

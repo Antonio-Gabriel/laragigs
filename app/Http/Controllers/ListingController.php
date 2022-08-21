1466257ecb56e964b3c5186087a17435181d1bac
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-08-21 15:54:50 +0100

    feat: add store and create job view

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Listing;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+// use Illuminate\Support\Facades\Session;
 
 class ListingController extends Controller
 {
@@ -10,7 +13,8 @@ class ListingController extends Controller
     public function index()
     {
         return view('listings.index', [
-            "listings" => Listing::latest()->filter(request(['tag', 'search']))->get()
+            "search" => request("search") ?? null,
+            "listings" => Listing::latest()->filter(request(['tag', 'search']))->paginate(6)
         ]);
     }
 
@@ -20,5 +24,34 @@ class ListingController extends Controller
         return view("listings.show", [
             "listing" => $listing
         ]);
+    }
+
+    // Form to create listings
+    public function create()
+    {
+        return view("listings.create");
+    }
+
+    public function store(Request $request)
+    {
+        $formField = $request->validate([
+            'tags' => 'required',
+            'title' => 'required',
+            'website' => 'required',
+            'location' => 'required',
+            'description' => 'required',
+            'email' => ['required', 'email'],
+            'company' => ['required', Rule::unique('listings', 'company')],
+        ]);
+
+        if ($request->hasFile('logo')) {
+            $formField['logo'] = $request->file('logo')->store('logos', 'public');
+        }
+
+        Listing::create($formField);
+
+        // Session::flash('message', 'Listing created successfully');
+
+        return redirect('/')->with('message', 'Listing created successfully');
     }
 }

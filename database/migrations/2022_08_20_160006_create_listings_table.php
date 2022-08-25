af31dba1e730cd49eeab094e0f7c41725ce2f8b8
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-08-25 17:34:30 +0100

    feat: add relationship on migration

--- database/migrations/2022_08_20_160006_create_listings_table.php
+++ database/migrations/2022_08_20_160006_create_listings_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('listings', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string("title");
             $table->string("logo")->nullable();
             $table->string("tags");
@@ -26,7 +27,7 @@ return new class extends Migration
             $table->timestamps();
         });
     }
- 
+
     /**
      * Reverse the migrations.
      *

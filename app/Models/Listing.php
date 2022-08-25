66e14a34281d2318607323655186fa349e580438
Antonio Gabriel <antoniocamposgabriel@gmail.com>
2022-08-25 17:33:53 +0100

    feat: add relationship

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
@@ -30,5 +31,11 @@ class Listing extends Model
                 ->orWhere('tags', 'like', '%' . request('search') . '%')
                 ->orWhere('description', 'like', '%' . request('search') . '%');
         }
+    }
+
+    // Relationship To User
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
     }
 }
